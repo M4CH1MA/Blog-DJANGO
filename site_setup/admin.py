ab9531e9991d41a59a29042d302de9398358e529
M4CH1MA <jonathandallavecchia23@gmail.com>
2024-04-27 21:30:40 -0300

    alteracoes nos models

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from django.http import HttpRequest
+from site_setup.models import MenuLink, SiteSetup
 
 # Register your models here.
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+    inlines = MenuLinkInline,
+
+    #metodo para ver se ja tem 1 registo no admin do django
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.all()[:1].exists()
